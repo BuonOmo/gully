54c572fd59ac7b6a79587b6d9d36b454e1a60d2b
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2017-04-20 21:45:36 -0400

    fix ticketing (??)

---
+++ src/pages/Ticketing.vue
@@ -0,0 +1,23 @@
+<template>
+    <main-layout>
+        <iframe id="haWidget" src="https://www.helloasso.com/associations/artfact/evenements/festival-gully-sound-5/widget"
+                style="width:800px;height:750px;border:none;"></iframe>
+    </main-layout>
+</template>
+
+<script>
+  import MainLayout from '../layouts/Main.vue'
+
+  export default {
+    components: {
+      MainLayout
+    }
+  }
+</script>
+
+<style>
+    #haWidget {
+        display: block;
+        margin: auto;
+    }
+</style>
