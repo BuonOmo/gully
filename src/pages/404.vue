cbd775e7c90a241ce87b35e69f65340cf6d85a92
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2017-05-22 16:17:00 -0400

    update 404

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,6 +1,9 @@
 <template>
   <main-layout>
-    <p>La page que tu cherches n'existe pas, viens plutôt voir la <v-link href="/programmation">programmation</v-link> !</p>
+    <main>
+      <h1><span>4</span><span>0</span><span>4</span></h1>
+      <p>La page que tu cherches n'existe pas...</p>
+    </main>
   </main-layout>
 </template>
 
@@ -17,8 +20,26 @@
 </script>
 
 <style lang="sass" scoped>
-  p
-    margin: 30% auto auto
+  main
+    margin: auto
     display: block
     max-width: 600px
+    padding-top: 17%
+    text-align: center
+    h1
+      font-size: 8em
+      margin-bottom: 0
+      >span
+        font-family: title
+      @for $i from 0 through 2
+        >span:nth-of-type(#{$i + 1})
+          -webkit-animation: blur 2s $i*0.3s 41 alternate
+          -o-animation: blur 2s $i*0.3s 41 alternate
+          animation: blur 2s $i*0.3s 41 alternate
+
+  @keyframes blur
+    from
+      filter: blur(8px)
+    to
+      filter: none
 </style>
