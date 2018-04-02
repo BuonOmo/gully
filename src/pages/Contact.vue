2d08e5958edabff916f09641b2d1b1572deeabb2
BuonOmo <buonomo.ulysse@gmail.com>
2018-04-02 23:57:53 +0200

    use detail layout for contact page

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,41 +1,19 @@
 <template>
-    <main-layout>
-        <div class="main-content">
-            <div>
-                <p>
-                    Pour demander des informations, venir jouer au Gully Sound, devenir bénévole ou pour toutes autres
-                    demandes, contactez-nous !
-                    <br><a href="mailto:asso.artfact@gmail.com">asso.artfact@gmail.com</a>
-                </p>
-            </div>
-        </div>
-    </main-layout>
+    <detail-layout>
+        <p>
+            Pour demander des informations, venir jouer au Gully Sound, devenir bénévole ou pour toutes autres
+            demandes, contactez-nous !
+            <br><a href="mailto:asso.artfact@gmail.com">asso.artfact@gmail.com</a>
+        </p>
+    </detail-layout>
 </template>
 
 <script>
-  import MainLayout from '../layouts/Main.vue'
+  import DetailLayout from '../layouts/Detail.vue'
 
   export default {
     components: {
-      MainLayout
+      DetailLayout
     }
   }
 </script>
-
-<style lang="sass" scoped>
-    .main-content
-        display: flex
-        flex-wrap: wrap
-        width: 50%
-        font-size: xx-large
-        margin: auto
-        text-align: center
-        padding-top: 15%
-        >div
-            width: 50%
-            &:first-child
-                width: 100%
-
-    .smaller
-        font-size: large
-</style>
