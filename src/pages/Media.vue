990225b025394c9fff4619cfda82eb9196eb6f9b
BuonOmo <buonomo.ulysse@gmail.com>
2018-04-03 18:45:37 +0200

    clean photo page and add thumbnails

--- src/pages/Media.vue
+++ src/pages/Media.vue
@@ -1,16 +1,13 @@
 <template>
   <detail-layout>
-    <template v-for="photoGroup in photosInformation.reverse()">
+    <template v-for="photoGroup in photosInformation">
       <h1>{{photoGroup.text}}, {{photoGroup.date}}</h1>
-      <!-- the tabindex is just here for the element to be focusable -->
-      <img class="small-picture"
-           tabindex="1"
-           v-for="n in photoGroup.count"
-           :data-id="`${photoGroup.edition}_${n}`"
-           :src="loadImage(photoGroup.edition, n)"
-           :alt="`GULLY_${photoGroup.edition}_${n}.jpg`"
-           :width="getWidth(photoGroup.edition, n)">
-      <!--@click="showImage(photoGroup.edition, n)"-->
+      <div class="grid-photo-container">
+        <img class="small-picture"
+             v-for="n in photoGroup.count"
+             :src="loadImage(photoGroup.edition, n)"
+             :alt="`GULLY_${photoGroup.edition}_${n}.jpg`">
+      </div>
     </template>
   </detail-layout>
 </template>
@@ -18,17 +15,11 @@
 <script>
   import DetailLayout from '../layouts/Detail.vue'
   import photosInformation from '../img/photos/index.yml'
-  import Vue from 'vue'
 
   export default {
   	data() {
-      const w = photosInformation.map(({count}) =>
-        new Array(count).fill(290)
-      )
-
       return {
-      	photosInformation,
-        w
+      	photosInformation: photosInformation.reverse()
       }
     },
 
@@ -38,23 +29,36 @@
 
     methods: {
       loadImage(edition, number) {
-        return require(`../img/photos/GULLY_${edition}_${number}.jpg`)
-      },
-
-      showImage(edition, number) {
-      	// https://vuejs.org/v2/guide/list.html#Array-Change-Detection
-      	this.w[edition-1].splice(number - 1, 1, 900)
-      	// this.w[edition-1][number] = 900
-      },
-
-      getWidth(edition, number) {
-      	return document.activeElement.getAttribute('data-id') === `${edition}_${number}` ? 900 : 290
+        return require(`../img/photos/thumbnails/GULLY_${edition}_${number}.jpg`)
       }
     }
   }
 </script>
 
 <style lang="sass">
-  .small-picture
-    margin: 2px
+
+  $photo-margin: 5px
+
+  .grid-photo-container
+    /* Prevent vertical gaps */
+    line-height: 0
+
+    column-count: 4
+    column-gap: $photo-margin
+
+    > img
+      /* Just in case there are inline attributes */
+      width: 100%
+      height: auto
+      margin-bottom: $photo-margin
+
+  @media (max-width: 1000px)
+    .grid-photo-container
+      column-count: 3
+  @media (max-width: 800px)
+    .grid-photo-container
+      column-count: 2
+  @media (max-width: 400px)
+    .grid-photo-container
+      column-count: 1
 </style>
