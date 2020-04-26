fa681ea966f6e9c3853db35c1fd1712ea3c29679
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2020-04-26 18:18:20 +0200

    update programmation with covid communiqué

--- src/pages/Programmation.vue
+++ src/pages/Programmation.vue
@@ -4,14 +4,136 @@
     <iframe width="560" height="315" src="https://www.youtube.com/embed/orJTg2nTRrw" frameborder="0"
     allowfullscreen></iframe>
 
-     <h1>Programmation a venir...</h1>
+    <h1>Communique — Festival Gully Sound</h1>
+
+    <p>En ces temps incertains nous souhaitons partager ces réflexions avec vous :</p>
+    <p>
+    💥🌱 Habituellement le printemps est synonyme d’excitation pour les amateurs de festivals. Le
+    lancement des programmations, les rêves de son roadtrip estival, les premières vibes en extérieur,
+    les découvertes musicales entre amis, les premières bières au soleil, la nature qui s’éveille.
+    Malheureusement c’est une toute autre réalité qui brutalement nous est imposée.
+    La crise que nous traversons nous rappelle notre fragilité et les limites de nos
+    modes de vie. Elle s’annonce d’ores et déjà longue et profonde tant elle touche
+    l’ensemble de nos activités. Il nous faudra s’adapter et coconstruire avec
+    cette crise en insufflant au cœur de chaque projet des valeurs sociales et écologiques.
+    Nous espérons que nous en tirerons le meilleur individuellement et collectivement. Il y a
+    tant de chose à ré-imaginer et nos événements récréatifs en font partie.
+    </p>
+    <h2>👏💪 A propos du secteur culturel : <em>May the force be with you</em></h2>
+    <p>
+    Nous avons mis volontairement notre communication en suspens depuis l’arrivée du coronavirus en
+    France. C’est arrivé au moment où nous comptions se lancer dans le grand bain comme tant d'autres
+    événements. Nous avons souhaité patienter avant de prendre position. Le monde de la Culture est
+    fortement impacté. Nous sommes de tout cœur avec ces acteurs qui nous procurent plaisirs, émotions
+    et évasions toute l’année. La communauté artistique fait d’ailleurs preuve d’innovation et nous
+    offre des plaisirs quotidiens pendant ce confinement. Merci !
+    </p>
+    <h2>🤞☀ Notre position pour le Festival Gully Sound #8 : <em>Si nous croisons tous les doigts ça passera !</em></h2>
+
+    <p>Nous avons décidé d’y croire et de maintenir le festival.</p>
+    <p>
+    Nous espérons qu’un déconfinement progressif et qu’un retour évolutif à la « normale » permettra la
+    tenue de la dernière édition du Gully les 16, 17 et 18 juillet prochains. Dans le cas où les mesures
+    restrictives aux rassemblements sont toujours de vigueur début juin ou si la sécurité des festivaliers
+    ne serait pas encore garantie en juillet nous prendrons la décision d'un éventuel report ou d'une annulation
+    complète pour 2020.
+    </p>
+    <p>
+    Dans la mesure du possible nous maintiendrons le festival comme il sera annoncé prochainement mais il est
+    possible que des changements surviennent, tels que :
+    </p>
+    <ul>
+      <li>
+        La venue des artistes internationaux : qui auxquels cas seront suppléés par des artistes situés en France/dans la région.
+      </li>
+      <li>
+        La jauge possible des festivaliers : si elle amenée à diminuer (décisions des institutions), cela engendra une hausse du prix
+        des places pour assurer un équilibre financier. Nous espérons que vous comprendrez cette mesure.
+      </li>
+    </ul>
+    <p>
+      🍂🍦🚀 En attendant un peu d'espoir et de couleurs. C’est avec joie que nous vous dévoilons le visuel de cette huitième
+      et dernière édition du plus fondant et délicieux des sound-systems reggae du Bugey ! 3 jours d’évasion à déguster dans
+      notre cocon forestier à Arandas (01). Un dernier voyage vers l’infini et au-delà...
+    </p>
+    <p>
+      🛠 L’équipe du Gully continue à travailler dans cette période d’incertitude pour vous offrir de
+      nouvelles surprises et maintenir la tenue du festival.
+    </p>
+    <p>
+      ❤ Protégez-vous, votre famille et vos amis, chaque jour nos retrouvailles sont plus proches. ❤ Un grand merci
+      à tous ceux qui continuent de maintenir notre société debout !
+    </p>
+
+    <p>❤Avec tout notre amour ! 🔥 L’équipe du Gully</p>
+
+    <img src="../img/posters/900/8.jpg" alt="La huitième édition du Gully !">
+
+
+    <h1>Appel à candidature — Scene ouverte</h1>
+
+    <p>
+      😎 Amateur.rice.s de musique Oyé Oyé ! Tu tâtes le vinyl le samedi soir avec les copains,
+      tu passes tes nuits à apprendre la MAO, tu profites du confinement pour te mettre à l'accordéon, tu
+      prévois de chanter à ton balcon à la prochaine fête de la musique, tu rêves d'une première expérience
+      de jeu en public ? Ce formulaire est pour toi ! Publication à faire tourner sans modération !
+    </p>
+    <p>
+      🔥 Pour la huitième et dernière édition du Gully Sound, l'association souhaite permettre à tout.e
+      amateur.trice de musique de venir partager son univers. C'est pourquoi nous proposons 8 créneaux
+      de 30 min pour 8 groupes/artistes sur la scène secondaire avec la sono Amahoro Sound & Records. Les
+      inscriptions peuvent se faire jusqu'au 6 JUIN 2020. Nous reviendrons vers vous pour la sélection finale.
+    </p>
+    <p>
+      ⚠ Cette scène ouverte ne s'adresse en AUCUN CAS aux artistes professionnels qui jouent régulièrement dans
+      le cadre d'une prestation payante. Les temps de jeu proposés ici ne sont PAS
+      RÉMUNÉRÉS. Nous n'avons pas les moyens de répartir nos finances dans toutes les pistes de diversifications
+      du festival. Il n'est pas question ici de profiter gratuitement de la performance d'artistes confirmés mais
+      bel et bien de proposer un espace et un temps de jeu à des amateurs qui habituellement n'ont pas la possibilité
+      d'accéder à ces moments. L'idée est d'offrir l'opportunité d'une première expérience de jeu.
+    </p>
+    <p>CARACTERISTIQUES :</p>
+    <ul>
+      <li>
+        4 artistes sélectionnés pour le vendredi soir et 4 pour le samedi soir (préférence à définir)
+      </li>
+      <li>
+        Tous les styles musicaux sont encouragés. Venez avec des couleurs, de l'originalité et le max d'énergie pour faire danser votre public ! Seuls les projets un "peu trop hardcore" (acide, trance, etc.) seront refusés. De manière personnelle nous aimons beaucoup ces styles mais la forme actuelle du Gully se dessine dans un certain cadre musical, nous espérons que vous comprendrez cette démarche.
+      </li>
+      <li>
+        30 min de set (on sait c'est court..)
+      </li>
+      <li>
+        Pas de rémunération ni de défraiement, uniquement un PASS 3J pour l'ensemble du festival
+      </li>
+      <li>
+        Technique : voir fiche technique (mode sound system) matériel mis à disposition par Amahoro Sound System
+      </li>
+    </ul>
+    <p>
+      💝 À la suite des sets joués il y aura 2 prix décernés avec un '"humble coffret cadeau" :
+    </p>
+    <ul>
+      <li>1 prix sélection "Gully Crew"</li>
+      <li>1 prix "coup de coeur du public" (niveau sonore)</li>
+    </ul>
+
+    <p><a href="https://forms.gle/LSXJzCGqesVnvkYg7" target="_blank" rel="noopener noreferrer">
+      Par ici pour le concours!
+    </a></p>
+
+    <p><a href="https://www.facebook.com/events/1411526555671778/" target="_blank" rel="noopener noreferrer">
+      👉 Festival Gully Sound #8 - Dernière édition - 2020
+    </a></p>
+
+    <!-- <h1>Programmation a venir...</h1>
     <p class="justify">
     D'ici quelques temps, tu trouveras ici la programmation pour les soirées du jeudi 16, vendredi
     17 et samedi 18 juillet. Ainsi que toutes les activités durant le festival.
     Pour être le premier au courant, n'hésite pas à suivre
     <a href="https://www.facebook.com/events/1411526555671778/">la page facebook de l'évenement</a>! Et pour toutes
     les infos sur notre asso, c'est <a href="https://www.facebook.com/FestivalGullySound/">ici</a>.
-    </p>
+    </p> -->
   </detail-layout>
 </template>
 
